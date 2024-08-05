69d464d30dbd2c5ae9a6adbefff5c7e87dc2a238
Dilshodbek-git <yoqubjonovd2@gmail.com>
2024-08-05 18:02:23 +0500

    hamma kodlarni kommentga olib 1ta console ni ichiga console yozdim

--- leetcode.js
+++ leetcode.js
@@ -1,21 +1,23 @@
 
-var lengthOfLastWord = function (s) {
-  let arr = s.split(" ");
-  let myArr = arr.filter((el) => el != "");
-  let myArr2 = myArr.map((el) => el.length);
-  return myArr2[myArr2.length - 1];
-};
+// var lengthOfLastWord = function (s) {
+//   let arr = s.split(" ");
+//   let myArr = arr.filter((el) => el != "");
+//   let myArr2 = myArr.map((el) => el.length);
+//   return myArr2[myArr2.length - 1];
+// };
 
-console.log(lengthOfLastWord("   fly me   to   the moon  ")); // moon length 4
+// console.log(lengthOfLastWord("   fly me   to   the moon  ")); // moon length 4
 
-var deleteDuplicates = function (head) {
-  let arr = [];
-  for (let i = 0; i < head.length; i++) {
-    if (!arr.includes(head[i])) {
-      arr.push(head[i]);
-    }
-  }
-  return arr;
-};
+// var deleteDuplicates = function (head) {
+//   let arr = [];
+//   for (let i = 0; i < head.length; i++) {
+//     if (!arr.includes(head[i])) {
+//       arr.push(head[i]);
+//     }
+//   }
+//   return arr;
+// };
+console.log(console.log("2ta console.log"));
 
-console.log(deleteDuplicates([1, 2, 3, 3, 4, 4, 5]));
+
+// console.log(deleteDuplicates([1, 2, 3, 3, 4, 4, 5]));
